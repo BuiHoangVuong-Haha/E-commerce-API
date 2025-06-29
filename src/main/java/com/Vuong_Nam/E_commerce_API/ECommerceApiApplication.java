bc000db21d05fa1d557b825a01e7c0184b43d24f
BuiHoangVuong <buihoangvuong777@gmail.com>
2025-06-29 15:57:33 +0700

    Initial commit

---
+++ src/main/java/com/Vuong_Nam/E_commerce_API/ECommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.Vuong_Nam.E_commerce_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApiApplication.class, args);
+	}
+
+}
